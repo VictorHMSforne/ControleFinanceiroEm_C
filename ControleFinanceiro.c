6ce2cf18fb510af0e0cfc32b304771055f82cf9a
VictorHMSforne <vmendessforne@gmail.com>
2025-12-31 12:01:36 -0300

    Feito mais modificações

--- ControleFinanceiro.c
+++ ControleFinanceiro.c
@@ -2,24 +2,64 @@
 #include <stdlib.h>
 #include <locale.h>
 
-menu(){
-	printf("+----------------+\n");
-	printf("|      MENU      |\n");
-	printf("+----------------+\n");
-	
-	printf("\nSelecione uma opção: \n");
-	
-	printf("\n1 - Inserir um valor Gasto\n");
-	printf("2 - Inserir um valor de Entrada\n");
-	printf("3 - Relatório de Gastos\n");
-	printf("4 - Relatório de Entradas\n");
-	printf("5 - Sair\n");
-	
+void teste() {
+    system("cls");
+    printf("Testando!\n");
+    printf("Pressione ENTER para voltar...");
+    while (getchar() != '\n'); // limpa buffer
+    getchar();                 // espera ENTER
 }
 
-int main(){
-	setlocale(LC_ALL, "Portuguese");
-	
-	menu();
-	
+void menu() {
+    int opcao;
+
+    do {
+        do {
+            system("cls");
+            printf("+----------------+\n");
+            printf("|      MENU      |\n");
+            printf("+----------------+\n");
+
+            printf("\n1 - Inserir uma Despesa    (-)\n");
+            printf("2 - Inserir um Recebimento (+)\n");
+            printf("3 - Relatório de Despesas\n");
+            printf("4 - Relatório de Recebimento\n");
+            printf("5 - Relatório Geral\n");
+            printf("6 - Sair\n");
+
+            printf("\nOpção: ");
+            scanf("%d", &opcao);
+
+            while (getchar() != '\n'); // limpa ENTER do buffer
+
+            if (opcao < 1 || opcao > 6) {
+                printf("Opção inválida!\n");
+                printf("Pressione ENTER para continuar...");
+                getchar();
+            }
+
+        } while (opcao < 1 || opcao > 6);
+
+        switch (opcao) {
+            case 1:
+                teste();
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+        }
+
+    } while (opcao != 6);
 }
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+    menu();
+    return 0;
+}
+
